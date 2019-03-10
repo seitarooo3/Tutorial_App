34173c41c976c04bfb75d9ce647454b880e52c33
Seinosuke Aoki <seinosuke.aoki@gmail.com>
2019-03-10 12:56:08 +0000

    Finish Attendances Show Page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,24 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    
+    if params[:first_day].nil?
+      @first_day = Date.today.beginning_of_month
+    else
+      @first_day = Date.parse(params[:first_day])
+    end
+    @last_day = @first_day.end_of_month
+    @dates = @user.attendances.where('worked_on >= ? and worked_on <= ?', @first_day, @last_day).order('worked_on')
+    (@first_day..@last_day).each do |day|
+    unless @user.attendances.any? {|attendance| attendance.worked_on == day}
+      record = @user.attendances.build(worked_on: day)
+      record.save
+    end
+    
+    @worked_sum = @dates.where.not(started_at: nil).count
+    
+  end
+
   end
   
   def new
